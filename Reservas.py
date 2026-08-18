a9098aea28cf1fc501b06ebb5476ae933496d2b5
FramirezBlancDempf <framirezblanc@uade.edu.ar>
2026-08-18 16:57:26 -0300

    Update Reservas.py

--- Reservas.py
+++ Reservas.py
@@ -1,3 +1,6 @@
+precio=5000
+
+
 def reservas():
     horarios=["08:00","09:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00", "18:00","19:00","20:00","21:00","22:00","23:00"]
     for i in range(len(horarios)):
@@ -14,5 +17,5 @@ def reservas():
             print("Su horario ha sido reservado con éxito.")
         else:
             print("No se ha realizado ninguna reserva.")
-            
+
 reservas()
