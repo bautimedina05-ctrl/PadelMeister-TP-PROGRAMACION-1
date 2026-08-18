be582b233796f21ffc3d720442683bf571a37978
FramirezBlancDempf <framirezblanc@uade.edu.ar>
2026-08-18 16:37:17 -0300

    Create Reservas.py

---
+++ Reservas.py
@@ -0,0 +1,7 @@
+def reservas():
+    horarios=["08:00","09:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00", "18:00","19:00","20:00","21:00","22:00","23:00"]
+    for i in range(len(horarios)):
+        print(i, horarios[i])
+    horario = int(input("seleccione el horario que desea reservar: "))
+    
+reservas()
