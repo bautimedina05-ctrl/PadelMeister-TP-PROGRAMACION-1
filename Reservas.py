425290e488bfc8d3a11cb6a89000ceaf8a790272
bautimedina05-ctrl <bautimedina05@gmail.com>
2026-08-19 00:14:00 -0300

    update reserva
    Desarrolle la parte de si esta disponible, ocupado y la parte de seleccion del turno

--- Reservas.py
+++ Reservas.py
@@ -1,21 +1,26 @@
-precio=5000
+def reserva_cancha(reservado, disponible):
+    print("Los horarios disponibles para su reserva son: ")
+    for i in range(len(disponible)):
+        print(i, "- ", disponible[i])
+    seleccionada = int(input("Ingrese el numero de horario que desee: "))
+    horario = disponible[seleccionada]
+    reservado.insert(seleccionada, horario)
+    del disponible[seleccionada]
+    return horario
+
+def horarios_canchas():
+    reservado=[]
+    disponible=["16:00","17:00", "18:00","19:00","20:00","21:00","22:00","23:00", "00:00"]
+    horario= reserva_cancha(reservado, disponible)
+    print("El horario seleccionado es: " , horario)
+    print("el resto de horarios es: ", disponible)
+    print("el resto de horarios es: ", reservado)
+    
+
+def alquiler_cancha():
+    matriz=[["Horario", "Estado", "Nombre Reserva"]]
+    horario= horarios_canchas()
+    
 
 
-def reservas():
-    horarios=["08:00","09:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00", "18:00","19:00","20:00","21:00","22:00","23:00"]
-    for i in range(len(horarios)):
-        print(i, horarios[i])
-    horario = int(input("seleccione el horario que desea reservar: "))
-    reservados=[]
-    if horario in reservados:
-        print("El horario ya está reservado. Por favor, seleccione otro horario.")
-    else:
-        print("El importe por el horario seleccionado es de: .", precio)
-        x = input("¿Desea reservar este horario? Y=0/N=1: ")
-        if x == "0":
-            reservados.append(horario)
-            print("Su horario ha sido reservado con éxito.")
-        else:
-            print("No se ha realizado ninguna reserva.")
-
-reservas()
+alquiler_cancha()
