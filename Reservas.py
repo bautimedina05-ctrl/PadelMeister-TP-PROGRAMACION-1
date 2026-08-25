39407f0783a57590a6988190a057797498161e44
FramirezBlancDempf <framirezblanc@uade.edu.ar>
2026-08-25 13:52:47 -0300

    Update Reservas.py

--- Reservas.py
+++ Reservas.py
@@ -5,6 +5,7 @@
 #Funciones de Reserva
 
 def reserva_cancha(reservado, disponible):
+    """Permite seleccionar un horario disponible para reservar una cancha. Muestra los horarios libres, recibe la elección del usuario, agrega el horario seleccionado a la lista de reservados, lo elimina de la lista de disponibles y devuelve el horario elegido."""
     print("Los horarios disponibles para su reserva son: ")
     for i in range(len(disponible)):
         print(i, "- ", disponible[i])
@@ -15,11 +16,13 @@ def reserva_cancha(reservado, disponible):
     return horario
 
 def nombre_reserva(hora):
+    """Permite ingresar el nombre del usuario para registrar su reserva."""
     nombre= input("Ingrese su nombre para registrar su reserva a las "+ hora + " :")
     return nombre
     
 
 def alquiler_cancha(matriz,reservado, disponible):
+    """Permite realizar una reserva de cancha. Llama a las funciones reserva_cancha y nombre_reserva para obtener el horario y el nombre del usuario, y luego agrega esta información a la matriz de reservas."""
     horario= reserva_cancha(reservado, disponible)
     nombre= nombre_reserva(horario)
     matriz.append([horario, "Ocupado", nombre])
@@ -28,6 +31,7 @@ def alquiler_cancha(matriz,reservado, disponible):
 #Mostrar alquileres
 
 def mostrar_reservas(matriz):
+    """Muestra todas las reservas realizadas en la matriz de reservas."""
     print("------RESERVAS------")
     for i in range(len(matriz)):
         print(matriz[i][0], " | ", matriz[i][1], " | ", matriz[i][2])
@@ -36,6 +40,7 @@ def mostrar_reservas(matriz):
 #Opciones ALQUILER
 
 def opciones_alquiler():
+    """Muestra un menú de opciones relacionadas con el alquiler de canchas. Permite al usuario elegir entre agregar un alquiler, ver las reservas existentes o volver al menú principal."""
     lista= ["Agregar Alquiler", "Reservas", "Volver Atras"]
     print("--ELIJA OPCION DESEADA--")
     for i in range(len(lista)):
@@ -56,6 +61,7 @@ def opciones_alquiler():
 #Funciones Menu
 
 def presentar_opciones():
+    """Muestra un menú principal con opciones disponibles para el usuario. Permite al usuario elegir entre diferentes secciones del sistema."""
     lista= ["Alquiler CANCHA", "TIENDA", "SOCIOS", "TORNEOS", "RANKING"]
     print("--ELIJA OPCION DESEADA--")
     for i in range(len(lista)):
