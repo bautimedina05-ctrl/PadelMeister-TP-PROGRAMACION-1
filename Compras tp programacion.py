e4ab5a0f4a99c0d681263de5e547c80dc2019570
tsimonetta1 <tsimonetta@uade.edu.ar>
2026-08-31 21:51:13 -0300

    Avance 1 ventas

--- Compras tp programacion.py
+++ Compras tp programacion.py
@@ -1,6 +1,31 @@
-def busca_productos(codigo):
+def busca_productos(productos):
     """Busca productos por código."""
+    posicion= -1
+    codigo= int(input("Ingrese el código del producto: "))
+    for i in range(len(productos)):
+        if codigo == productos[i]:
+            posicion= i
+    return posicion
 
 
 
 #MAIN
+productos=[23]
+precios=[4]
+cantidad=[32]
+print("Bienvenido al sistema de compras")
+print("1. Buscar productos por código")
+print("2. Ingresar nuevo producto: ")
+print("3. Eliminar producto: ")
+print("4. Salir del sistema: ")
+opcion= int(input("Ingrese la opcion que decida realizar: "))
+if opcion == 1:
+    codigo= busca_productos(productos)
+    if codigo != -1:
+        print("Codigo", "Precio", "Cantidad")
+        print(productos[codigo],"    ", precios[codigo],"     ",cantidad[codigo])
+    else:
+        print("Codigo no encontrado")
+
+
+    
