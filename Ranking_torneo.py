a17ab0e569274677dc1e553d14f154c6976d2594
FramirezBlancDempf <framirezblanc@uade.edu.ar>
2026-09-08 13:33:15 -0300

    listas

--- Ranking_torneo.py
+++ Ranking_torneo.py
@@ -2,8 +2,9 @@ from socios import socios
 ranking_individual = []
 ranking_dobles = []
 
-# Lista de socios válidos para prueba
-SOCIOS_VALIDOS = ["101", "102", "103", "104", "105"]
+#se importa la lista de socios desde el archivo socios.py para poder verificar si un socio es válido al momento de inscribirse en los rankings.
+
+SOCIOS_VALIDOS = socios
 
 
 def socio(socios_validos):
