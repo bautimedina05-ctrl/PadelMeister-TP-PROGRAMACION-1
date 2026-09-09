0b394705f6eee7101af25875c828d00ce999481f
Fedegt06 <ftittonell@uade.edu.ar>
2026-09-08 22:32:40 -0300

    Update Ranking_torneo.py

--- Ranking_torneo.py
+++ Ranking_torneo.py
@@ -22,11 +22,14 @@ def validar_numero_socio():
 def socio(socios_validos):
     """Función para ingresar el número de socio y verificar si es válido."""
     nro_socio = validar_numero_socio()
-    while nro_socio not in socios_validos:
+    if nro_socio not in socios_validos:
         print("---------------------------------------------------------------------------------------")
         print("Número de socio inexistente. Por favor, ingrese un número válido.")
         print("---------------------------------------------------------------------------------------")
-        nro_socio = validar_numero_socio()
+    else:
+        print("---------------------------------------------------------------------------------------")
+        print(f"Bienvenido, socio {nro_socio}.")
+        print("---------------------------------------------------------------------------------------")
     return nro_socio
 
 
